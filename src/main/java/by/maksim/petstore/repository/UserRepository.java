a55de5cb1a75ae76174c49ae25a18634bdd228bf
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-11-15 17:01:26 +0300

    Added UserRepository. Added Maven dependency: Spring Boot Starter Data JPA (v.2.4.0).

---
+++ src/main/java/by/maksim/petstore/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package by.maksim.petstore.repository;
+
+import by.maksim.petstore.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
