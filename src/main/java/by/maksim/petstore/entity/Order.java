8a9e8172f96ad50aa976bc071881afa21c0c621c
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-11-19 18:38:30 +0300

    Updated entities. Updated Category. Updated Order. Updated Pet. Updated Tag. Updated User.

--- src/main/java/by/maksim/petstore/entity/Order.java
+++ src/main/java/by/maksim/petstore/entity/Order.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue
     private int id;
     private int petId;
     private int quantity;
--- src/main/java/by/maksim/petstore/entity/Pet.java
+++ src/main/java/by/maksim/petstore/entity/Pet.java
@@ -4,23 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pet {
+    @Id
+    @GeneratedValue
     private int id;
     @NotEmpty
     @NotBlank
+    @OneToOne
     private Category category;
     @NotEmpty
     @NotBlank
     private String name;
     @NotEmpty
     @NotBlank
+    @OneToMany
     private List<Tag> tags;
     @NotEmpty
     @NotBlank
